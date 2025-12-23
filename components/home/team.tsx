e869311dd842856a09eb7185bbe5fa3caac5535d
pouyaesmaili <pouryaesmaieli@gmail.com>
2025-12-23 13:14:48 +0330

    add team component

---
+++ components/home/team.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import Link from "next/link";
+import Row from "../global/Row";
+
+const Team = () => {
+    return (
+        <div className="w-full">
+
+            <div className="flex flex-col items-center gap-10 py-10 px-6 md:hidden">
+                <div className="w-full">
+                    <Image
+                        width={1500}
+                        height={1500}
+                        src="/images/landing/Group.svg"
+                        alt="team"
+                        className="w-full h-auto object-cover"
+                    />
+                </div>
+
+                <div className="w-full flex flex-col justify-between text-left">
+                    <h2 className="my-8 text-2xl font-medium text-black">
+                        Behind the <mark className='bg-[#FFE600]'>Nexa</mark>
+                    </h2>
+
+                    <p className="text-justify text-[18px] text-black">
+                        NEXA is an international team with 10+ years of hands-on experience in launching and scaling businesses globally. Our expertise spans Global Marketing Strategy, Product Development, Advanced R&D, Branding & Graphic Design, UI/UX Design, Content Strategy, and Go-to-Market Execution, with a strong focus on execution.
+                    </p>
+                    <br />
+                    <p className='text-justify text-[18px] text-black'>
+                        We deliver investment-ready strategies through close collaboration with international consultants and legal experts, helping founders turn early-stage ideas into scalable, market-ready companies.
+                    </p>
+
+                    <Link
+                        href="#"
+                        className="mt-7 h-12 w-45 border border-[#8F27FF] text-[#8F27FF] hover:bg-[#8F27FF] hover:text-white flex items-start justify-start rounded-3xl px-6 py-3 text-base transition-colors duration-300"
+                    >
+                        Meet Our Team →
+                    </Link>
+                </div>
+            </div>
+
+            <div className="hidden md:block">
+                <Row>
+                    <div className="flex flex-row items-center gap-18">
+                        {/* Text */}
+                        <div className="w-1/2 flex flex-col justify-between text-left">
+                            <h2 className="my-8 text-[32px] font-medium text-black">
+                                Behind the <mark className='bg-[#FFE600]'>Nexa</mark>
+                            </h2>
+
+                            <p className="text-justify text-[18px] text-black">
+                                NEXA is an international team with 10+ years of hands-on experience in launching and scaling businesses globally. Our expertise spans Global Marketing Strategy, Product Development, Advanced R&D, Branding & Graphic Design, UI/UX Design, Content Strategy, and Go-to-Market Execution, with a strong focus on execution.
+                            </p>
+                            <br />
+                            <p className='text-justify text-[18px] text-black'>
+                                We deliver investment-ready strategies through close collaboration with international consultants and legal experts, helping founders turn early-stage ideas into scalable, market-ready companies.
+                            </p>
+
+                            <Link
+                                href="#"
+                                className="mt-7 h-12 w-max border border-[#8F27FF] text-[#8F27FF] hover:bg-[#8F27FF] hover:text-white flex items-center justify-center rounded-3xl px-6 py-3 text-base transition-colors duration-300"
+                            >
+                                Meet Our Team →
+                            </Link>
+                        </div>
+
+                        <div className="w-1/2">
+                            <Image
+                                width={1500}
+                                height={1500}
+                                src="/images/landing/Group.svg"
+                                alt="team"
+                                className="w-full h-auto object-cover"
+                            />
+                        </div>
+                    </div>
+                </Row>
+            </div>
+
+        </div>
+    );
+};
+
+export default Team;

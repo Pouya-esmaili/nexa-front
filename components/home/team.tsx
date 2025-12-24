9984018a86a638b84b9b56992ebfaec08659fa8f
pouyaesmaili <pouryaesmaieli@gmail.com>
2025-12-24 11:45:54 +0330

    update team section

--- components/home/team.tsx
+++ components/home/team.tsx
@@ -18,7 +18,7 @@ const Team = () => {
                 </div>
 
                 <div className="w-full flex flex-col justify-between text-left">
-                    <h2 className="my-8 text-2xl font-medium text-black">
+                    <h2 className="my-8 text-2xl font-bold text-black">
                         Behind the <mark className='bg-[#FFE600]'>Nexa</mark>
                     </h2>
 
@@ -32,7 +32,7 @@ const Team = () => {
 
                     <Link
                         href="#"
-                        className="mt-7 h-12 w-45 border border-[#8F27FF] text-[#8F27FF] hover:bg-[#8F27FF] hover:text-white flex items-start justify-start rounded-3xl px-6 py-3 text-base transition-colors duration-300"
+                        className="mt-7 h-12 w-46 border border-[#8F27FF] text-medium font-medium text-[#8F27FF] hover:bg-[#8F27FF] hover:text-white flex items-start justify-start rounded-3xl px-6 py-3 text-base transition-colors duration-300"
                     >
                         Meet Our Team →
                     </Link>
@@ -44,21 +44,21 @@ const Team = () => {
                     <div className="flex flex-row items-center gap-18">
                         {/* Text */}
                         <div className="w-1/2 flex flex-col justify-between text-left">
-                            <h2 className="my-8 text-[32px] font-medium text-black">
+                            <h2 className="my-8 text-3xl font-bold text-black">
                                 Behind the <mark className='bg-[#FFE600]'>Nexa</mark>
                             </h2>
 
-                            <p className="text-justify text-[18px] text-black">
+                            <p className="text-[20px] text-black">
                                 NEXA is an international team with 10+ years of hands-on experience in launching and scaling businesses globally. Our expertise spans Global Marketing Strategy, Product Development, Advanced R&D, Branding & Graphic Design, UI/UX Design, Content Strategy, and Go-to-Market Execution, with a strong focus on execution.
                             </p>
                             <br />
-                            <p className='text-justify text-[18px] text-black'>
+                            <p className='text-[20px] text-black'>
                                 We deliver investment-ready strategies through close collaboration with international consultants and legal experts, helping founders turn early-stage ideas into scalable, market-ready companies.
                             </p>
 
                             <Link
                                 href="#"
-                                className="mt-7 h-12 w-max border border-[#8F27FF] text-[#8F27FF] hover:bg-[#8F27FF] hover:text-white flex items-center justify-center rounded-3xl px-6 py-3 text-base transition-colors duration-300"
+                                className="mt-7 h-12 w-max border border-[#8F27FF] font-medium text-[#8F27FF] hover:bg-[#8F27FF] hover:text-white flex items-center justify-center rounded-3xl px-6 py-3 text-base transition-colors duration-300"
                             >
                                 Meet Our Team →
                             </Link>

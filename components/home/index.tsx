1c5f2bcb1d4712c541dc9ed7c6b09b7ac8bf3889
pouyaesmaili <pouryaesmaieli@gmail.com>
2025-12-24 11:45:45 +0330

    update hero section

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -5,13 +5,13 @@ const Hero = () => {
     return (
         <div className="w-full">
 
-            <div className="flex flex-col gap-6 px-6 mt-10     md:hidden">
-                <h2 className="text-2xl font-medium text-black">
+            <div className="flex flex-col gap-6 px-6 mt-10 md:hidden">
+                <h2 className="text-3xl font-bold text-black">
                     Your{" "}
                     <mark className="bg-[#FFE52C]">Global Launchpad</mark>{" "} for Investment, Startups, Entrepreneurship, and Career Opportunities
                 </h2>
 
-                <p className="text-[16px] text-justify">
+                <p className="text-[20px] text-justify">
                     We fuel the global expansion of startups into new markets — for
                     entrepreneurs ready to scale and investors seeking high-growth
                     opportunities.
@@ -31,8 +31,8 @@ const Hero = () => {
                     <div className="flex items-center gap-18">
                         {/* Text */}
                         <div className="w-1/2 text-left flex flex-col justify-between">
-                            <h2 className="my-8 text-[32px] font-medium text-black">Your{" "}<mark className="bg-[#FFE52C]">Global Launchpad</mark>{" "}for Investment, Startups, Entrepreneurship, and Career Opportunities</h2>
-                            <p className="text-[18px] text-justify">
+                            <h2 className="my-8 text-[32px] font-bold text-black">Your{" "}<mark className="bg-[#FFE52C]">Global Launchpad</mark>{" "}for<br></br> Investment, Startups,<br></br> Entrepreneurship, and Career Opportunities</h2>
+                            <p className="text-[24px]">
                                 We fuel the global expansion of startups into new markets — for
                                 entrepreneurs ready to scale and investors seeking high-growth
                                 opportunities.

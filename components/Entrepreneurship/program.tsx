22a5e08a22298fd7501eb4ae80a43e893aafd9a5
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-02-02 15:24:29 +0330

    add program section

---
+++ components/Entrepreneurship/program.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import Row from "../global/Row";
+
+const program = () => {
+    return (
+        <div className="w-full">
+
+            {/* Mobile */}
+            <div className="flex flex-col gap-6 px-6 mt-10 md:hidden">
+                <h2 className="text-3xl font-bold text-black">
+                    What is the Entrepreneurship?
+                </h2>
+
+                <p className="text-[20px] text-justify">
+                    Entrepreneurship programs offered by different countries are designed to attract innovative founders, investors,
+                    and business owners who can contribute to economic growth, job creation, and innovation. These programs provide
+                    structured pathways for entrepreneurs to launch, expand, or relocate their businesses by offering access to new
+                    markets, resources, and supportive regulatory frameworks. Governments implement these programs to strengthen
+                    their economies, encourage foreign investment, and remain competitive in an increasingly globalized world.
+                </p>
+
+                {/* Purple line */}
+                <div className="w-24 h-1 bg-[#8F27FF]" />
+
+                {/* Bullet items */}
+                {/* <div className="flex flex-col gap-4 mt-4">
+                    {[
+                        "Market entry strategy and localization",
+                        "Business development and partnerships",
+                        "Investor-ready structure and scaling",
+                    ].map((text, index) => (
+                        <div key={index} className="flex items-start gap-3">
+                            <Image
+                                src="/images/Entrepreneurship/done.svg"
+                                alt="done"
+                                width={20}
+                                height={20}
+                                className="mt-1"
+                            />
+                            <p className="text-[18px] text-justify">{text}</p>
+                        </div>
+                    ))}
+                </div> */}
+
+                <Image
+                    width={1200}
+                    height={1200}
+                    src="/images/Entrepreneurship/program.svg"
+                    alt="hero"
+                    className="w-full h-auto object-contain"
+                />
+            </div>
+
+            {/* Desktop */}
+            <div className="hidden md:block">
+                <Row>
+                    <div className="flex items-center gap-18 mt-20">
+                        {/* Image */}
+                        <div className="w-1/2">
+                            <Image
+                                width={1500}
+                                height={1500}
+                                src="/images/Entrepreneurship/program.svg"
+                                alt="hero"
+                                className="w-full h-80 object-cover"
+                            />
+                        </div>
+
+                        {/* Text */}
+                        <div className="w-1/2 text-left flex flex-col justify-between">
+                            <h2 className="text-3xl font-bold text-black">
+                                What is the Entrepreneurship?
+                            </h2>
+                            <div className="w-68 h-[3px] bg-[#8F27FF] mb-4 mt-8" />
+                            <p className="text-lg">
+                                Entrepreneurship programs offered by different countries are designed to attract innovative founders, investors,
+                                and business owners who can contribute to economic growth, job creation, and innovation. These programs provide
+                                structured pathways for entrepreneurs to launch, expand, or relocate their businesses by offering access to new
+                                markets, resources, and supportive regulatory frameworks. Governments implement these programs to strengthen
+                                their economies, encourage foreign investment, and remain competitive in an increasingly globalized world.
+                            </p>
+                        </div>
+                    </div>
+                </Row>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default program;

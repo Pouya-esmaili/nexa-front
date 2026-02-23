8c552ff7934772f194cd732ece3260cfdcb7e532
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-02-23 10:54:43 +0330

    update entrepreneurship page

--- app/Entrepreneurship/page.tsx
+++ app/Entrepreneurship/page.tsx
@@ -1,24 +1,30 @@
 import Branch from "@/components/Entrepreneurship/branch";
+import Global from "@/components/Entrepreneurship/global";
 import Service from "@/components/Entrepreneurship/service";
 import Program from "@/components/Entrepreneurship/program";
-import Country from "@/components/Entrepreneurship/country";
+// import Country from "@/components/Entrepreneurship/country";
 import FAQSection from "@/components/Entrepreneurship/faqSection";
 import EntrepreneurshipSection from "@/components/Entrepreneurship/entrepreneurshipSection";
 import TargetAudienceSection from "@/components/Entrepreneurship/targetAudienceSection";
 import Previous from "@/components/Entrepreneurship/previous";
+import Form from "@/components/home/form";
+import Designe from "@/components/Entrepreneurship/designe";
 
 export default function Entrepreneurship() {
     return (
         <>
             <div className="w-full bg-white sm:items-star">
                 <Branch />
-                <Service />
-                <Country />
+                <Global />
+                <Service />          
+                {/* <Country /> */}
                 <Program />
+                <Designe />
                 <EntrepreneurshipSection />
-                <TargetAudienceSection />
+                {/* <TargetAudienceSection /> */}
                 <Previous />
                 <FAQSection />
+                <Form />
             </div>
         </>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,41 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {
+
       colors: {
         highlight: "#FFE52C",
+
+        // اضافه شده برای کارت services
+        serviceCard: "#F7F6F9",
+
+        // بنفش کنار کارت
+        servicePurple: "#8F27FF",
+
+        // بک آیکون
+        iconBg: "#FFFFFF",
       },
+
+      backdropBlur: {
+        30: "30px",
+      },
+
+      boxShadow: {
+
+        // shadow قبلی
+        card: "0px 10px 15px 0px #00000040",
+
+        // shadow services card
+        serviceCard: "0px 4px 4px 0px #00000040",
+
+        // shadow icon
+        serviceIcon: "0px 4px 4px 0px #00000040",
+      },
+
     },
   },
-  plugins: [],
+
+  plugins: [require('@tailwindcss/line-clamp')]
 };

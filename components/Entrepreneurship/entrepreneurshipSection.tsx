d3afced296d2ed10fafaaccc378a8893517ca9fb
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-02-23 11:24:36 +0330

    update section and style

--- components/Entrepreneurship/entrepreneurshipSection.tsx
+++ components/Entrepreneurship/entrepreneurshipSection.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Row from "../global/Row";
 
 const EntrepreneurshipSection = () => {
   const features = [
@@ -12,21 +11,22 @@ const EntrepreneurshipSection = () => {
   ];
 
   return (
-    <section className="py-16 mb-10 px-4 bg-gray-50">
+    <section className="py-16 mb-10">
       <div className="container mx-auto">
 
         <h2 className="text-2xl md:text-3xl font-bold text-center text-gray-900 mb-12">
           How Entrepreneurship Programs Strengthen Countries?
         </h2>
 
-        <Row className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* جایگزین Row با div گرید */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {features.map((item) => (
             <div
               key={item.id}
               className="flex items-center p-8 gap-6 bg-white rounded-2xl shadow-2xl hover:shadow-lg transition-shadow duration-300"
             >
 
-              <div className="flex-shrink-0 w-12 h-12 mr-6">
+              <div className="flex-shrink-0 w-12 h-12">
                 <Image
                   src={item.icon}
                   alt={item.title}
@@ -42,7 +42,7 @@ const EntrepreneurshipSection = () => {
               </h3>
             </div>
           ))}
-        </Row>
+        </div>
       </div>
     </section>
   );

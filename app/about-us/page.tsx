ed5ec29224a48765ffb5f55c27f318e6f480c89a
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-02-23 11:25:07 +0330

    update about us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,5 +1,15 @@
 import Story from "@/components/About/story";
-import TimelineSection from "@/components/About/TimelineSection";
+import VisionMission from "@/components/About/visionmission";
+import AcademyTimeline from "@/components/About/academyTimeline";
+import DiacoTimeline from "@/components/About/diacoTimeline";
+import VisionTimeline from "@/components/About/visionTimeline";
+import TripTimeline from "@/components/About/tripTimeline";
+import InnovationTimeline from "@/components/About/innovationTimeline";
+import PreventigeneTimeline from "@/components/About/diacoTimeline";
+import AutilabTimeline from "@/components/About/autilabTimeline";
+import LandacraftTimeline from "@/components/About/landacraftTimeline";
+import Certificat from "@/components/About/certificate";
+// import TimelineSection from "@/components/About/TimelineSection";
 
 
 export default function AboutUs() {
@@ -7,7 +17,17 @@ export default function AboutUs() {
     <>
       <div className="w-full bg-white sm:items-start">
         <Story />
-        <TimelineSection />
+        <VisionMission />
+        <AcademyTimeline />
+        <DiacoTimeline />
+        <VisionTimeline />
+        <TripTimeline />
+        <InnovationTimeline />
+        <PreventigeneTimeline />
+        <AutilabTimeline />
+        <LandacraftTimeline />
+        <Certificat />
+        {/* <TimelineSection /> */}
       </div>
     </>
   );

e4eafdaaa6ccd2567f78a222e780651c0150ad2e
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-05-31 13:36:55 +0430

    update index section

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
           <div className="flex items-center gap-16">
             {/* Text */}
             <div className="w-1/2 flex flex-col gap-6">
-              <h1 className="text-[42px] font-bold leading-snug text-black">
+              <h1 className="text-3xl lg:text-4xl xl:text-[42px] font-bold leading-snug text-black">
                 Your <Highlight>Global Launchpad</Highlight> for
                 <br />
                 Investment, Startups,
@@ -42,7 +42,7 @@ const Hero = () => {
                 Entrepreneurship, and Career Opportunities
               </h1>
 
-              <p className="text-[22px] text-black font-normal text-justify">
+              <p className="text-base lg:text-lg xl:text-[22px] text-black font-normal text-justify">
                 We fuel the global expansion of startups into new markets — for
                 entrepreneurs ready to scale and investors seeking high-growth
                 opportunities.

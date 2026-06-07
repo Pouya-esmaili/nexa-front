d1ee18767e5e2680d192359a6ba5cf58da624ebd
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-06-07 21:23:35 +0430

    refactor HomePage component for improved structure and organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,27 @@
-import Image from "next/image";
-import Home from "@/components/home";
+import Hero from "@/components/home";
+import Ticker from "@/components/home/section";
 import Feature from "@/components/home/feature";
-import Section from "@/components/home/section";
 import Bio from "@/components/home/bio";
 import Results from "@/components/home/results";
 import ProjectsSection from "@/components/home/projects";
 import Comments from "@/components/home/comments";
 import Choose from "@/components/home/choose";
 import Team from "@/components/home/team";
-// import Touch from "@/components/home/touch";
 import Form from "@/components/home/form";
 
 export default function HomePage() {
   return (
-
-    <main className=" w-full bg-white sm:items-start">
-      <h2 className="text-black"></h2>
-      <Home />
+    <main className="w-full bg-white">
+      <Hero />
+      <Ticker />
       <Feature />
-      <Section />
       <Bio />
       <Results />
       <ProjectsSection />
       <Comments />
       <Choose />
       <Team />
-      {/* <Touch /> */}
       <Form />
     </main>
-
   );
 }

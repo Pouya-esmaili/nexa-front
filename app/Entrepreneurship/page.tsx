d2be3429a1e43ad530a53a57031851de69eddd95
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-06-13 12:04:50 +0430

    update page Entrepreneurship

--- app/Entrepreneurship/page.tsx
+++ app/Entrepreneurship/page.tsx
@@ -1,31 +1,33 @@
-import Branch from "@/components/Entrepreneurship/branch";
-import Global from "@/components/Entrepreneurship/global";
-import Service from "@/components/Entrepreneurship/service";
-import Program from "@/components/Entrepreneurship/program";
-// import Country from "@/components/Entrepreneurship/country";
-import FAQSection from "@/components/Entrepreneurship/faqSection";
-import EntrepreneurshipSection from "@/components/Entrepreneurship/entrepreneurshipSection";
-import TargetAudienceSection from "@/components/Entrepreneurship/targetAudienceSection";
-import Previous from "@/components/Entrepreneurship/previous";
-import Form from "@/components/home/form";
-import Designe from "@/components/Entrepreneurship/designe";
+import Hero from "@/components/Entrepreneurship/Hero";
+import StatsBand from "@/components/Entrepreneurship/StatsBand";
+import Markets from "@/components/Entrepreneurship/Markets";
+import Services from "@/components/Entrepreneurship/Services";
+import WhatIs from "@/components/Entrepreneurship/WhatIs";
+import ForWhom from "@/components/Entrepreneurship/ForWhom";
+import Impact from "@/components/Entrepreneurship/Impact";
+import Projects from "@/components/Entrepreneurship/Projects";
+import FAQ from "@/components/Entrepreneurship/FAQ";
+import ContactForm from "@/components/Entrepreneurship/ContactForm";
 
-export default function Entrepreneurship() {
-    return (
-        <>
-            <div className="w-full bg-white sm:items-star">
-                <Branch />
-                <Global />
-                <Service />          
-                {/* <Country /> */}
-                <Program />
-                <Designe />
-                <EntrepreneurshipSection />
-                {/* <TargetAudienceSection /> */}
-                <Previous />
-                <FAQSection />
-                <Form />
-            </div>
-        </>
-    );
+export const metadata = {
+  title: "Nexa | Entrepreneurship",
+  description:
+    "Launch your next business branch in the global market. Nexa guides ambitious founders to scale globally through government-backed entrepreneurship programs in Canada, Spain, Finland, Portugal, and the Netherlands.",
+};
+
+export default function EntrepreneurshipPage() {
+  return (
+    <div className="w-full bg-white">
+      <Hero />
+      <StatsBand />
+      <Markets />
+      <Services />
+      <WhatIs />
+      <ForWhom />
+      <Impact />
+      <Projects />
+      <FAQ />
+      <ContactForm />
+    </div>
+  );
 }

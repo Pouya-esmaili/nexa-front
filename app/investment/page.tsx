e60cfbcca0ae03a64653e516ba9930863f17a737
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-06-13 12:44:38 +0430

    update page investment

--- app/investment/page.tsx
+++ app/investment/page.tsx
@@ -1,25 +1,29 @@
-import Invest from "@/components/investment/invest";
-import Offer from "@/components/investment/offer";
-import Value from "@/components/investment/value";
-import FAQSection from "@/components/Entrepreneurship/faqSection";
-import Form from "@/components/home/form";
-import PreviousProjectsSection from "@/components/Entrepreneurship/previous";
-import InvestSection from "@/components/investment/investSection";
-import Process from "@/components/investment/process";
+import InvestmentHero from "@/components/investment/hero";
+import InvestmentStatsBand from "@/components/investment/statsBand";
+import InvestmentCountries from "@/components/investment/offer";
+import InvestmentServices from "@/components/investment/value";
+import InvestmentWhatIs from "@/components/investment/whatIs";
+import InvestmentProcess from "@/components/investment/process";
+import InvestmentSectors from "@/components/investment/sectors";
+import InvestmentImpact from "@/components/investment/impact";
+import InvestmentProjects from "@/components/investment/projects";
+import InvestmentFAQ from "@/components/investment/faq";
+import InvestmentContactForm from "@/components/investment/contactForm";
 
-export default function investment() {
+export default function InvestmentPage() {
     return (
-        <>
-            <div className="w-full bg-white sm:items-start">
-                <Invest />
-                <Offer />
-                <Value />
-                <InvestSection />
-                <Process />
-                <PreviousProjectsSection />
-                <FAQSection />
-                <Form />
-            </div>
-        </>
+        <div className="w-full bg-white overflow-x-hidden">
+            <InvestmentHero />
+            <InvestmentStatsBand />
+            <InvestmentCountries />
+            <InvestmentServices />
+            <InvestmentWhatIs />
+            <InvestmentProcess />
+            <InvestmentSectors />
+            <InvestmentImpact />
+            <InvestmentProjects />
+            <InvestmentFAQ />
+            <InvestmentContactForm />
+        </div>
     );
 }

34d80083f6101f48da1b5c8b322681399550206f
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-06-16 17:47:59 +0430

    update feature section

--- components/home/feature.tsx
+++ components/home/feature.tsx
@@ -85,29 +85,29 @@ function Card({
 }) {
   return (
     
-    <div className="group relative w-full h-full flex flex-col items-center transition-all duration-500 mt-10 md:mt-6 origin-top hover:scale-y-90">
+    <div className="group relative w-full h-full flex flex-col items-center transition-all duration-500 mt-8 md:mt-6 origin-top hover:scale-y-90">
       {/* Card Shadow Wrapper */}
       <div className="relative w-full h-full filter drop-shadow-[0_-1px_0_#8F27FF] group-hover:drop-shadow-none transition-all duration-300">
 
         {/* Card Body */}
-        <div className="relative w-full h-[320px] bg-white border border-[#8F27FF] rounded-4xl p-5 pt-16 pb-20 flex flex-col items-center text-center transition-all duration-500 group-hover:bg-[#8F27FF] group-hover:translate-y-4 [mask-image:radial-gradient(circle_at_top_center,transparent_2.8rem,black_2.9rem)] [-webkit-mask-image:radial-gradient(circle_at_top_center,transparent_2.8rem,black_2.9rem)]">
+        <div className="relative w-full h-[260px] bg-white border border-[#8F27FF] rounded-4xl p-4 pt-12 pb-14 flex flex-col items-center text-center transition-all duration-500 group-hover:bg-[#8F27FF] group-hover:translate-y-4 [mask-image:radial-gradient(circle_at_top_center,transparent_2.8rem,black_2.9rem)] [-webkit-mask-image:radial-gradient(circle_at_top_center,transparent_2.8rem,black_2.9rem)]">
 
           {/* Title */}
-          <h3 className="absolute top-10 left-1/2 pt-3 -translate-x-1/2 text-[24px] font-bold text-black mb-4 transition-all duration-500 ease-in-out group-hover:top-1/2 group-hover:-translate-y-1/2 group-hover:text-white">
+          <h3 className="absolute top-8 left-1/2 pt-2 -translate-x-1/2 text-[20px] font-bold text-black mb-3 transition-all duration-500 ease-in-out group-hover:top-1/2 group-hover:-translate-y-1/2 group-hover:text-white">
             {title}
           </h3>
 
           {/* Description */}
-          <div className="flex-1 flex items-start overflow-hidden pt-3 transition-all duration-500 max-h-[200px] opacity-100 group-hover:max-h-0 group-hover:opacity-0 mt-6">
-            <p className="text-[16px] text-gray-400 font-normal leading-relaxed mb-12">
+          <div className="flex-1 flex items-start overflow-hidden pt-2 transition-all duration-500 max-h-[160px] opacity-100 group-hover:max-h-0 group-hover:opacity-0 mt-6">
+            <p className="text-[14px] text-gray-400 font-normal leading-relaxed mb-10">
               {desc}
             </p>
           </div>
 
           {/* Arrow Button */}
-          <div className="absolute bottom-6 right-6">
-            <div className="w-10 h-10 rounded-full flex items-center justify-center bg-[#8F27FF] group-hover:bg-white transition-all cursor-pointer">
-              <Image src="/images/landing/ArrowRight.svg" alt="Arrow" width={20} height={20} className="group-hover:invert transition-all" />
+          <div className="absolute bottom-4 right-4">
+            <div className="w-9 h-9 rounded-full flex items-center justify-center bg-[#8F27FF] group-hover:bg-white transition-all cursor-pointer">
+              <Image src="/images/landing/ArrowRight.svg" alt="Arrow" width={16} height={16} className="group-hover:invert transition-all" />
             </div>
           </div>
         </div>
@@ -115,8 +115,8 @@ function Card({
 
       {/* Top Icon */}
       <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-[8px] z-10 transition-transform duration-500 group-hover:translate-y-[10px]">
-        <div className="w-28 h-4 flex items-center justify-center">
-          <Image src={icon} alt={title} width={80} height={80} />
+        <div className="w-20 h-4 flex items-center justify-center">
+          <Image src={icon} alt={title} width={64} height={64} />
         </div>
       </div>
     </div>

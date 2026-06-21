e45cf27c8da11b98f012fd0cf2ddaa305a00db9a
pouyaesmaili <pouryaesmaieli@gmail.com>
2026-06-21 13:52:18 +0430

    refactor: adjust padding for title in Card component for improved layout

--- components/home/feature.tsx
+++ components/home/feature.tsx
@@ -93,7 +93,7 @@ function Card({
         <div className="relative w-full h-[260px] bg-white border border-[#8F27FF] rounded-4xl p-4 pt-12 pb-14 flex flex-col items-center text-center transition-all duration-500 group-hover:bg-[#8F27FF] group-hover:translate-y-4 [mask-image:radial-gradient(circle_at_top_center,transparent_2.8rem,black_2.9rem)] [-webkit-mask-image:radial-gradient(circle_at_top_center,transparent_2.8rem,black_2.9rem)]">
 
           {/* Title */}
-          <h3 className="absolute top-8 left-1/2 pt-2 -translate-x-1/2 md:text-[24px] text-[22px] font-bold text-black mb-3 transition-all duration-500 ease-in-out group-hover:top-1/2 group-hover:-translate-y-1/2 group-hover:text-white">
+          <h3 className="absolute top-8 left-1/2 pt-3 -translate-x-1/2 md:text-[24px] text-[22px] font-bold text-black mb-3 transition-all duration-500 ease-in-out group-hover:top-1/2 group-hover:-translate-y-1/2 group-hover:text-white">
             {title}
           </h3>
 
